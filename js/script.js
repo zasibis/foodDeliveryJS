3385de5d799c526df3ea5033df5625d0fe1be106
user <user@users-MacBook-Air.local>
2022-10-25 19:03:21 +0300

    work with modal and scroll

--- js/script.js
+++ js/script.js
@@ -27,7 +27,7 @@ window.addEventListener('DOMContentLoaded', () => {
     showTabContent();
 
     tabsParent.addEventListener('click', (event) => {
-        
+
         const target = event.target;
         if (target && target.classList.contains('tabheader__item')) {
             tabs.forEach((item, i) => {
@@ -43,12 +43,12 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const deadline = '2023-05-30';
 
-    function getTimeRemaining () {
-        const t = Date.parse(deadline) - Date.parse(new Date),
-              days = Math.floor(t / (1000 * 60 * 60 * 24)),
-              hours = Math.floor((t / (1000 * 60 * 60)) % 24),
-              minutes = Math.floor((t / (1000 * 60)) % 60),
-              seconds = Math.floor((t / 1000) % 60);
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date),
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+            minutes = Math.floor((t / (1000 * 60)) % 60),
+            seconds = Math.floor((t / 1000) % 60);
 
         return {
             'total': t,
@@ -59,23 +59,23 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    function getZero(num){
-        if (num >= 0 && num < 10) { 
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
             return '0' + num;
         } else {
             return num;
         }
     }
 
-    function setCLock (selector, endtime) {
+    function setCLock(selector, endtime) {
         const timer = document.querySelector(selector),
-              days = timer.querySelector('#days'),
-              hours = timer.querySelector('#hours'),
-              minutes = timer.querySelector('#minutes'),
-              seconds = timer.querySelector('#seconds'),
-              interval = setInterval(updateClock, 1000);
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            interval = setInterval(updateClock, 1000);
 
-              updateClock()
+        updateClock()
 
         function updateClock() {
             const t = getTimeRemaining(endtime);
@@ -85,11 +85,60 @@ window.addEventListener('DOMContentLoaded', () => {
             minutes.innerHTML = getZero(t.minutes);
             seconds.innerHTML = getZero(t.seconds);
 
-            if (t.total <= 0 ) {
+            if (t.total <= 0) {
                 clearInterval(interval);
             }
         }
     }
 
     setCLock('.timer', deadline);
+
+
+    // Modal
+
+    const modalsCLose = document.querySelector('[data-close]'),
+        modalsShow = document.querySelectorAll('[data-modal]'),
+        modal = document.querySelector('.modal');
+
+    function opendModal() {
+        modal.classList.toggle('show');
+        document.body.style.overflow = 'hidden';
+        clearInterval(modalTimerId);
+    }
+
+    function modalsClose() {
+        modal.classList.toggle('show');
+        document.body.style.overflow = '';
+    }
+
+    modalsShow.forEach(btns => {
+        btns.addEventListener('click', opendModal)
+    });
+
+
+    modalsCLose.addEventListener('click', modalsClose);
+
+    modal.addEventListener('click', (e) => {
+        if (e.target == modal) {
+            modalsClose();
+        }
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code == 'Escape' && modal.classList.contains('show')) {
+            modalsClose();
+        }
+    });
+
+    const modalTimerId = setTimeout(opendModal, 5000);
+
+    function showModelByScroll () {
+            if (window.pageYoffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight -1) {
+                opendModal();
+                window,removeEventListener('scroll', showModelByScroll);
+            }
+    }
+
+    window.addEventListener('scroll', showModelByScroll);
+
 });
