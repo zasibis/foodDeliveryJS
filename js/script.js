e61ae7ce94362ba4305b0b4e226aebd19d69b766
user <user@users-MacBook-Air.local>
2022-10-25 14:25:40 +0300

    Timer

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 "use strict";
 
 window.addEventListener('DOMContentLoaded', () => {
+
+    // TABS
     let tabs = document.querySelectorAll('.tabheader__item'),
         tabsParent = document.querySelector('.tabheader__items'),
         tabsContent = document.querySelectorAll('.tabcontent');
@@ -36,4 +38,58 @@ window.addEventListener('DOMContentLoaded', () => {
             })
         }
     })
+
+    //Timer
+
+    const deadline = '2023-05-30';
+
+    function getTimeRemaining () {
+        const t = Date.parse(deadline) - Date.parse(new Date),
+              days = Math.floor(t / (1000 * 60 * 60 * 24)),
+              hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+              minutes = Math.floor((t / (1000 * 60)) % 60),
+              seconds = Math.floor((t / 1000) % 60);
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds,
+        }
+    }
+
+    function getZero(num){
+        if (num >= 0 && num < 10) { 
+            return '0' + num;
+        } else {
+            return num;
+        }
+    }
+
+    function setCLock (selector, endtime) {
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              interval = setInterval(updateClock, 1000);
+
+              updateClock()
+
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total <= 0 ) {
+                clearInterval(interval);
+            }
+        }
+    }
+
+    setCLock('.timer', deadline);
 });
